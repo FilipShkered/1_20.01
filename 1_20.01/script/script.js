32cbb784b9f2d9eae13f44366f98f3bb3f8c95f3
Filip <example@gmail.com>
2023-01-26 00:52:39 +0200

    add style

--- 1_20.01/script/script.js
+++ 1_20.01/script/script.js
@@ -1,28 +1,37 @@
-function hello(){
-    console.log ('Hello World');
+// function hello(){
+//     console.log ('Hello World');
+// }
+
+// hello ();
+
+// function plus (a, b) {
+//     console.log (a + b);
+//     return a+b;
+// }
+
+// console.log (plus(4,4))
+// plus (2, 5);
+
+// function isfive(variable) {
+//     return variable === 5;
+// }
+
+// console.log(isfive(4));
+// console.log(isfive(5));
+
+// var stringName = 'Hello World';
+
+// function showText(text) {
+//     return text;
+// }
+
+// console.log (showText(stringName));
+
+function calculete(){
+
 }
+calculete();
 
-hello ();
-
-function plus (a, b) {
-    console.log (a + b);
-    return a+b;
+function showInputValue() {
+    document.getElementById()
 }
-
-console.log (plus(4,4))
-plus (2, 5);
-
-function isfive(variable) {
-    return variable === 5;
-}
-
-console.log(isfive(4));
-console.log(isfive(5));
-
-var stringName = 'Hello World';
-
-function showText(text) {
-    return text;
-}
-
-console.log (showText(stringName));
